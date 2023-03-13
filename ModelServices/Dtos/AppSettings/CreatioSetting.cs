8498dea26a9170aa36a1e534ce0c9a3ba160eea8
carlos arce <carlos.3101a@gmail.com>
2023-03-12 21:43:41 -0400

    adding creatio auth services

---
+++ ModelServices/Dtos/AppSettings/CreatioSetting.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ModelServices.Dtos.AppSettings
+{
+    public class CreatioSetting
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string UrlCreatio { get; set; }
+    }
+}
